b79160b5279585f670f9823e5fa6e56e53498e79
chrome-dino <42478360+chrome-dino@users.noreply.github.com>
2022-06-25 15:37:30 -0500

    Update pcap_virus_scan.py

--- src/pcap_virus_scan/pcap_virus_scan.py
+++ src/pcap_virus_scan/pcap_virus_scan.py
@@ -27,7 +27,7 @@ class PcapVirusScan():
                 os.remove(os.path.join(root,file))
 
         #use tshark to extract downloaded objects
-        process=subprocess.Popen('tshark -r ' + self.pcap + ' --export-objects ' + self.mode + ',\".\\pcap_virus_scan\\src\\pcap_virus_scan\\export_objects\"',shell=True)
+        process=subprocess.Popen('tshark -r ' + self.pcap + ' --export-objects ' + self.mode + ',\".\\pcap_virus_scan\\src\\pcap_virus_scan\\export_objects\"',shell=True,stdout=subprocess.PIPE)
         process.wait()
 
     def yara_mode(self):
