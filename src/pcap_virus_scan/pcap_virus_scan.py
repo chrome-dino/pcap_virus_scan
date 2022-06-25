6a680c55ee3b9bc5d12c9a75239610dbb4a7b19f
chrome-dino <42478360+chrome-dino@users.noreply.github.com>
2022-06-25 14:46:25 -0500

    Update pcap_virus_scan.py

--- src/pcap_virus_scan/pcap_virus_scan.py
+++ src/pcap_virus_scan/pcap_virus_scan.py
@@ -2,10 +2,12 @@ import os
 import hashlib
 from virus_total_apis import PublicApi as VirusTotalPublicApi
 import yara
+import subprocess
 
+EXECUTEABLE_EXT = ['.apk','.cmd','.run','.exe','.ipa','bin','.bat','.app','.cgi','.ps1','vbs']
 
 class PcapVirusScan():
-    def __init__(self, pcap, mode, key=None, rules=None, yara=False, virus_total=False):
+    def __init__(self, pcap, mode, key=None, rules=None, yara=False, virus_total=False,executables=False):
         # Intialize member variables
         self.pcap = pcap
         self.mode = mode
@@ -13,6 +15,7 @@ class PcapVirusScan():
         self.rules = rules
         self.yara = yara
         self.virus_total = virus_total
+        self.executables = executables
 
 
     def export_objects(self):
@@ -24,17 +27,22 @@ class PcapVirusScan():
                 os.remove(os.path.join(root,file))
 
         #use tshark to extract downloaded objects
-        os.system('cmd /k "tshark -r ' + self.pcap + ' --export-objects ' + self.mode + ', .\\pcap_virus_scan\\src\\pcap_virus_scan\\export_objects"')
+        process=subprocess.Popen('tshark -r ' + self.pcap + ' --export-objects ' + self.mode + ',\".\\pcap_virus_scan\\src\\pcap_virus_scan\\export_objects\"',shell=True)
+        process.wait()
 
-
-    def yara(self):
+    def yara_mode(self):
         rules = yara.compile(filepath=self.rules)
         print('##########################################################')
         print('YARA REPORT')
         print('##########################################################')
         print('')
-        for root, dirs, files in os.walk('.\export_objects'):
+        for root, dirs, files in os.walk('.\pcap_virus_scan\src\pcap_virus_scan\export_objects'):
             for file in files:
+                if self.executables:
+                    if os.path.splitext(file)[1] not in EXECUTEABLE_EXT:
+                        continue
+                if file == 'README':
+                    continue
                 print('----------------------------------------------------------')
                 print('Scan for File: ' + file)
                 print('----------------------------------------------------------')
@@ -44,26 +52,32 @@ class PcapVirusScan():
                     print('No matches detected')
                 else:
                     for match in matches:
-                        print('Rule: ' + match['rule'])
-                        if 'namespace' in match:
-                            print('Namespace: ' + match['namespace'])
+                        print('Rule: ' + match.rule)
+                        if match.namespace:
+                            print('Namespace: ' + match.namespace)
                         
-                        if 'tag' in match and match['tags']:
+                        if match.tags:
                             tag = ''
-                            for t in match['tags']:
+                            for t in match.tags:
                                 tag += t + ', '
 
                             print('Tags: ' + tag[:len(tag)-2])
 
-                        if 'meta' in match:
-                            print('Meta: ' + match['meta'])
+                        if match.strings:
+                            stri = ''
+                            for t in match.strings:
+                                stri += t[2].decode('ascii') + ', '
+
+                            print('Strings: ' + stri[:len(stri)-2])
+                        if match.meta:
+                            print('Meta: ' + match.meta)
                         
 
                 print('')
                 print('')
 
 
-    def virus_total(self,hashes):
+    def virus_total_mode(self,hashes):
         vt = VirusTotalPublicApi(self.key)
         print('##########################################################')
         print('VIRUS TOTAL REPORT')
@@ -74,41 +88,46 @@ class PcapVirusScan():
             if response['response_code'] != 200:
                 print('ERROR: Failed to get Virus Total report')
                 exit(-1)
+
             print('----------------------------------------------------------')
             print('Scan for File: ' + hash['filename'])
             print('Scan Date: ' + response['results']['scan_date'])
             print('MD5:' + response['results']['md5'])
             print('SHA256:' + response['results']['sha256'])
-            print('Detection Rate: ' + str(response['results']['positives']) + '/' + str(response['results']['negatives']))
+            print('Detection Rate: ' + str(response['results']['positives']) + '/' + str(response['results']['total']))
             print('----------------------------------------------------------')
-            for key in response['results']['scans'].keys:
+            for key in response['results']['scans']:
                 vt_submission = response['results']['scans'][key]
 
                 print('Source: ' + key)
-                print('Detected: ' + str(vt_submission['detected']))
+                print('\tDetected: ' + str(vt_submission['detected']))
                 if vt_submission['detected']:
-                    print('filename: ' + vt_submission['result'])
-                print('Update: ' + vt_submission['update'])
+                    print('\tfilename: ' + vt_submission['result'])
+                print('\tUpdate: ' + vt_submission['update'])
+                print('')
             print('')
             print('')
 
-    @staticmethod
-    def get_hash():
+    def get_hash(self):
         hashes = []
         for root, dirs, files in os.walk('.\\pcap_virus_scan\\src\\pcap_virus_scan\\export_objects'):
             for file in files:
                 if file == 'README':
                     continue
-                hashes.append({'filename':file,'md5':hashlib.md5(open(file,'rb').read()).hexdigest()})
+                if self.executables:
+                    if os.path.splitext(file)[1] not in EXECUTEABLE_EXT:
+                        continue 
+                hashes.append({'filename':file,'md5':hashlib.md5(open('.\\pcap_virus_scan\\src\\pcap_virus_scan\\export_objects\\' + file,'rb').read()).hexdigest()})
 
         return hashes
 
 
     def run(self):
+        self.export_objects()
         if self.virus_total:
-            hashes = self.export_object()
-            self.virus_total(hashes)
+            hashes = self.get_hash()
+            self.virus_total_mode(hashes)
         elif self.yara:
-            self.yara()
+            self.yara_mode()
             
         
